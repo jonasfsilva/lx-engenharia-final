63dcb6aa0938b74323c61ba6142f582ab85c6194
Jonas Ferreira <jonas.silva@grupomateus.com>
2025-03-19 16:43:01 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // screens: {
+    //   'sm': '820px',
+    //   // => @media (min-width: 640px) { ... }
+
+    //   'md': '1024px',
+    //   // => @media (min-width: 1024px) { ... }
+
+    //   'lg': '1280px',
+    //   // => @media (min-width: 1280px) { ... }
+    // },
+    extend: {},
+  },
+  plugins: [],
+};
